001a9a79de13c684b02c0b271a4e277f00c5b3a9
ConnerWit <97093629@st.deltion.nl>
2024-11-01 16:30:21 +0100

    alles

---
+++ practice.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+/*
+typedef std::vector<std::pair<std::string,int>> pairlist_t;
+//typedef std::string text_t;
+//typedef int int_t;
+
+using text_t = std::string;
+using int_t = int;
+*/
+
+/*
+namespace sigma{
+        int x = 2;
+    }
+    */
+
+int main()
+{
+
+    /*grade judging
+    char grade;
+    std::cout << "enter grade: ";
+    std::cin >> grade;
+
+    switch(grade) {
+        case 'A':
+            std::cout << "okayokay";
+            break;
+        case 'B':
+            std::cout << "okay";
+            break;
+        case 'C':
+            std::cout << "meh";
+            break;
+        case'D':
+            std::cout << "ass";
+            break;
+        case 'E':
+            std::cout << "assass";
+            break;
+        case 'F':
+            std::cout << "asssssssssss";
+            break;
+        default:
+            std::cout << "invalid";
+            break;
+    }
+    */
+
+    /*pythagorean theorem
+    int a;
+    int b;
+    double c;
+
+    std::cout << "enter a: ";
+    std::cin >> a;
+
+    std::cout << "enter b: ";
+    std::cin >> b;
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+
+    std::cout << "square root: " << c;
+    */
+
+    /*more math
+    double x = 3.99;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x, y);
+    //z = pow(2, 4);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(x);
+    //z = ceil(x);
+    z = floor(x);
+
+    std::cout << z;
+    */
+
+    /*user input
+    std::string name;
+    int age;
+
+    std::cout << "enter name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "age: ";
+    std::cin >> age;
+
+    std::cout << "ur age: " << age << '\n';
+    std::cout << "ur name: " << name << '\n';
+    */
+
+    /*type conversion
+    double x = (int) 5.5;;
+
+    std::cout << x << '\n';
+    std::cout << (char) 100 << '\n';
+
+    int correct = 8;
+    int questions = 100;
+    double percentage = (double)correct/questions * 100;
+
+    std::cout << percentage << "%" << '\n';
+    */
+
+    /*arythmatic
+    int students = 6 - 5 + 4 * 3 / 2;
+
+    std::cout<< students;
+    */
+
+    /*typedef/using
+    text_t firstname = "sigma";
+    int_t age = 2191114;
+    std::cout << age << std::endl;
+    */
+
+    /*namespace
+    using namespace sigma;
+    //int x = 1;
+    std::cout << x;
+    */
+
+    /*const
+    const double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    std::cout << circumference;
+    */
+
+    /*variables + basic data types
+    int year = 2020;
+    double pp = 1.1;
+    char initial = 'C';
+    bool dumb = true;
+    std::string name = "sigma";
+    */
+
+    /*strings and lines
+    std::cout << "Hello, Sigma 1!" << std::endl;
+    std::cout << "Hello, Sigma 2!" << '\n';
+    std::cout << "Hello, Sigma 3!";
+    */
+    return 0;
+}
