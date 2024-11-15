90fa09e5c6d771b323ddf27c6c97f9f4042f3157
ConnerWit <97093629@st.deltion.nl>
2024-11-15 17:55:58 +0100

    vgyuop

--- phonebook.cpp
+++ phonebook.cpp
@@ -2,34 +2,80 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <map>
 using namespace std;
 
-class BaseClass{
-	public:
-		virtual void input() = 0;
-		virtual void display() const = 0;
-		virtual ~BaseClass() = default;
-};
-
-class Contact : public BaseClass{
-	private:
-		int cNum;
-		string cName;
-	public:
-		void input() override {
-			cout << "enter contact num: ";
-			cin >> cNum;
-			cout << "enter contact name: ";
-			cin.ignore();
-			getline(cin, cName);
-		}
-		void display() const override {
-			cout << "contact:\n" << "number: " << cNum << endl << "name: " << cName << endl;
-		}
+struct Contact{
+	string name;
+	string phoneNum;
+	string email;
+	string photo;
 };
 
 int main(){
-	vector<shared_ptr<BaseClass>> objs;
+	map<string, Contact> phoneBook;
+	int choice = 0;
+	while(choice != 5){
+		cout << "menu" << endl;
+		cout << "1. add contact" << endl;
+		cout << "2. remove contact" << endl;
+		cout << "3. search contact" << endl;
+		cout << "4. display contact" << endl;
+		cout << "5. exit" << endl;
+		cout << "enter choice: ";
+		cin >> choice;
+		
+		if(choice == 1){
+			Contact newContact;
+			cout << "enter name: ";
+			cin >> newContact.name;
+			cout << "enter phone number: ";
+			cin >> newContact.phoneNum;
+			cout << "enter email: ";
+			cin >> newContact.email;
+			cout << "enter photo: ";
+			cin >> newContact.photo;
+			phoneBook[newContact.name] = newContact;
+			cout << "contact added" << endl;
+		} else if(choice == 2){
+			string nameToRemove;
+			cout << "enter name of contact to remove: ";
+			cin >> nameToRemove;
+			if(phoneBook.count(nameToRemove)){
+				phoneBook.erase(nameToRemove);
+				cout << "contact deleted" << endl;
+			} else {
+				cout << "name invalid" << endl;
+			}
+		} else if(choice == 3){
+			string nameToSearch;
+			cout << "enter name of contact: ";
+			cin >> nameToSearch;
+			if(phoneBook.count(nameToSearch)){
+				Contact contact = phoneBook[nameToSearch];
+				cout << "name: " << contact.name << endl;
+				cout << "phone number: " << contact.phoneNum << endl;
+				cout << "email: " << contact.email << endl;
+				cout << "photo: " << contact.photo << endl;
+			} else {
+				cout << "name invalid" << endl;
+			}
+		} else if(choice == 4){
+			for (const auto& pair : phoneBook) {
+    			const auto& name = pair.first;
+    			const auto& contact = pair.second;
+			
+    			cout << endl << "name: " << name << endl;
+    			cout << "phone number: " << contact.phoneNum << endl;
+    			cout << "email: " << contact.email << endl;
+    			cout << "photo: " << contact.photo << endl << endl;
+			}
+		} else if(choice == 5){
+			cout << "exiting phone book..." << endl;
+		} else {
+			cout << "choice invalid" << endl;
+		}
+	}
 	
 	return 0;
 }
