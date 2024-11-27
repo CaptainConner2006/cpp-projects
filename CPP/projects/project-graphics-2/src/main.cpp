4b6d1024c348399c3c0c41bff5af7d3386b2ce2c
ConnerWitTweeduizend <97093629@st.deltion.nl>
2024-11-27 11:46:20 +0100

    tdy

--- CPP/projects/project-graphics-2/src/main.cpp
+++ CPP/projects/project-graphics-2/src/main.cpp
@@ -2,35 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(){
-    if(!glfwInit()){
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+using namespace std;
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
-    if(!window){
-        std::cerr << "Failed to create window" << std::endl;
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    
+    if(window == NULL){
+        cout << "Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
-
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        glfwTerminate();
+        cout << "Failed to intiliaze GLAD\n";
         return -1;
     }
 
-    while(!glfwWindowShouldClose(window)){
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glViewport(0,0,800,600);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+        glViewport(0,0,width,height);
     }
+    
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    glfwTerminate();
     return 0;
 }
