1cfcfb384dd0dc202188e16642907e33b11c1409
ConnerWitTweeduizend <97093629@st.deltion.nl>
2024-12-18 14:13:03 +0100

    enums

--- C/projects/structs/main.c
+++ C/projects/structs/main.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 
-struct Struct
+enum State{WORKING, FAILED, FREEZED};
+enum State currState = 0;
+enum State findState()
 {
-  int id;
-  char name[];
-};
+  return currState;
+}
 
 int main()
 {
-  int size = 5;
-  struct Struct arrayStruct[size];
-
-  for(int i = 0; i < size; i ++)
-  {
-    arrayStruct[i].id = i + 1;
-    strcpy(arrayStruct[i].name, "User");
-  }
-  for(int i = 0; i < size; i++)
-  {
-    printf("Element %d: ID = %d, Name = %s\n", i + 1, 
-           arrayStruct[i].id, 
-           arrayStruct[i].name);
-  }
+  (findState() == WORKING)? printf("WORKING"): printf("NOT WORKING");
 
   return 0;
 }
